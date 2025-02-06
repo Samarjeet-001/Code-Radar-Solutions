dc84ebd2c1c85c5743f2b44db384dd56de1bff0b
Samarjeet Singh <samarjeetd09@gmail.com>
2025-02-06 17:27:19 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,6 +4,11 @@ int main()
     float a,b;
     char ch;
     scanf("%f %f %c",&a,&b,&ch);
+    if(b == 0 && ch == '/')
+    {
+        printf("error");
+        break;
+    }
     switch(ch)
     {
         case '+':
